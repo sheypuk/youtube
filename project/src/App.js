f6857d7fd17ffff884debc583ef853e184a0b3cd
sheypuk <78950211+sheypuk@users.noreply.github.com>
2021-12-08 08:01:13 +0200

    08.12

--- project/src/App.js
+++ project/src/App.js
@@ -3,18 +3,41 @@ import Counter from "./Components/Counter";
 import ClassCounter from "./Components/ClassCounter";
 import './Styles/app.css'
 import PostItem from "./Components/postItem";
+import PostList from "./Components/PostList";
+import Mybutton from "./Components/UI/button/mybutton";
+import Myinput from "./Components/UI/input/Myinput";
 
 function App() {
 
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'java', body: 'Desription'},
+        {id: 2, title: 'java2', body: 'Desription'},
+        {id: 3, title: 'java3', body: 'Desription'},
+    ])
 
-    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
 
+    const [title, setTitle] = useState('')
 
+    const addNewPost = (e) => {
+        e.preventDefault()
+    }
 
 
     return (
         <div className="App">
-            <PostItem post={{id:1, title: 'java', body: 'Desription'}}/>
+            <form>
+                {/*Управляемый компонент*/}
+                <Myinput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type='text'
+                    placeholder="Название поста"
+                />
+                <Myinput type='text' placeholder="описание поста"/>
+                <Mybutton onclick={addNewPost}>Создать пост</Mybutton>
+            </form>
+            <PostList posts={posts} title="Посты про JS"/>
+
 
         </div>
     )
