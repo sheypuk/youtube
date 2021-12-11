deca7a2e1a8725e7408a21a67816c24dade7209c
sheypuk <78950211+sheypuk@users.noreply.github.com>
2021-12-11 11:35:32 +0200

    11.12

--- project/src/App.js
+++ project/src/App.js
@@ -7,34 +7,55 @@ import PostList from "./Components/PostList";
 import Mybutton from "./Components/UI/button/mybutton";
 import Myinput from "./Components/UI/input/Myinput";
 import PostForm from "./Components/PostForm";
+import Myselect from "./Components/UI/select/Myselect";
 
 function App() {
 
     const [posts, setPosts] = useState([
-        {id: 1, title: 'java', body: 'Desription'},
-        {id: 2, title: 'java2', body: 'Desription'},
-        {id: 3, title: 'java3', body: 'Desription'},
+        {id: 1, title: 'java', body: 'Desription1'},
+        {id: 2, title: 'ara', body: 'Desription2'},
+        {id: 3, title: 'java3', body: 'Desription3'},
     ])
 
 
-
-const createPost = (newPost) => {
-        setPosts([...posts, newPost ])
-}
-
-const removePost =(post) => {
-        setPosts(posts.filter(p => p.id !==post.id))
-}
+    const [selectedSort, setSelectedSort] = useState('')
 
 
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        console.log(sort)
+        setPosts([...posts].sort((a,b) =>a[sort].localeCompare(b[sort])))
+    }
 
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            {posts.length !==0
-            ? <PostList remove={removePost} posts={posts} title="Посты про JS"/>
-            :
+            <hr style={{margin: '15px 0'}}/>
+            <div>
+                <Myselect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка"
+                    options={[
+                        {value: 'title', name: 'По названия'},
+                        {value: 'body', name: 'По описанию'}
+
+                    ]}
+                />
+            </div>
+            {posts.length
+                ?
+                <PostList remove={removePost} posts={posts} title="Посты про JS"/>
+                :
                 <h1 style={{textAlign: 'center'}}>
                     Посты не найдены
                 </h1>
